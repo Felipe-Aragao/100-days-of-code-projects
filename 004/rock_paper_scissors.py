f7daed726bb066330300ec2f0877427412105741
Felipe Aragão <lipelima2006@gmail.com>
2022-08-31 22:42:08 -0300

    update rock_paper_scissors.py

--- 004/rock_paper_scissors.py
+++ 004/rock_paper_scissors.py
@@ -2,29 +2,29 @@ import random
 
 
 options = ['rock', 'paper', 'scissor']
-
 choice = input('Chose "Rock" "Paper" or "Scissor": ').lower()
+index_user_option = 0
+
+try:
+    index_user_option = int(options.index(choice))
+except ValueError:
+    print('ERROR')
+    exit()
 
 random_option = random.randint(0, 2)
 computer_choice = options[random_option]
 
-if choice == 'rock' or choice == 'paper' or choice == 'scissor':
-    print(f'You chose {choice.swapcase()}')
-    print(f'Computer chose {computer_choice.swapcase()}\n')
-else:
-    print('ERROR')
+print(f'You chose {choice.swapcase()}')
+print(f'Computer chose {computer_choice.swapcase()}\n')
 
-if computer_choice == choice:
-    print('Draw')
-elif computer_choice == 'rock' and choice == 'scissor':
-    print('You lose!')
-elif computer_choice == 'paper' and choice == 'rock':
-    print('You lose!')
-elif computer_choice == 'scissor' and choice == 'paper':
-    print('You lose!')
-elif choice == 'rock' and computer_choice == 'scissor':
-    print('You win!')
-elif choice == 'paper' and computer_choice == 'rock':
-    print('You win!')
-elif choice == 'scissor' and computer_choice == 'paper':
-    print('You win!')
+if index_user_option == 0 and random_option == 2:
+    print("You win!")
+elif random_option == 0 and index_user_option == 2:
+    print("You lose")
+elif random_option > index_user_option:
+    print("You lose")
+elif index_user_option > random_option:
+    print("You win!")
+elif random_option == index_user_option:
+    print("It's a draw")
+
