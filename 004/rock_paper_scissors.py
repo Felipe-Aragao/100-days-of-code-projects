022954fd020bb34ec25b85aa1819b9aed1b2856a
Felipe Aragão <lipelima2006@gmail.com>
2022-09-01 14:43:30 -0300

    update variable name

--- 004/rock_paper_scissors.py
+++ 004/rock_paper_scissors.py
@@ -1,30 +1,28 @@
 import random
 
-
 options = ['rock', 'paper', 'scissor']
 choice = input('Chose "Rock" "Paper" or "Scissor": ').lower()
 index_user_option = 0
 
 try:
-    index_user_option = int(options.index(choice))
+    index_user_option = options.index(choice)
 except ValueError:
     print('ERROR')
     exit()
 
-random_option = random.randint(0, 2)
-computer_choice = options[random_option]
+computer_choice = random.randint(0, 2)
+computer_choice_str = options[computer_choice]
 
 print(f'You chose {choice.swapcase()}')
-print(f'Computer chose {computer_choice.swapcase()}\n')
+print(f'Computer chose {computer_choice_str.swapcase()}\n')
 
-if index_user_option == 0 and random_option == 2:
+if index_user_option == 0 and computer_choice == 2:
     print("You win!")
-elif random_option == 0 and index_user_option == 2:
+elif computer_choice == 0 and index_user_option == 2:
     print("You lose")
-elif random_option > index_user_option:
+elif computer_choice > index_user_option:
     print("You lose")
-elif index_user_option > random_option:
+elif index_user_option > computer_choice:
     print("You win!")
-elif random_option == index_user_option:
+elif computer_choice == index_user_option:
     print("It's a draw")
-
