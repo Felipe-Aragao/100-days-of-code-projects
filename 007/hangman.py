7ec64afd689041022b70ab67bb80f9a9c4a35d32
Felipe Aragão <lipelima2006@gmail.com>
2022-09-04 17:33:15 -0300

    made the game playable

--- 007/hangman.py
+++ 007/hangman.py
@@ -1,21 +1,34 @@
 import random
 
+# Create initial variables and lists
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-display = list('_' * len(chosen_word))
 chosen_word_list = list(chosen_word)
+display = list('_' * len(chosen_word))
+lives = 5
+# Debug print()
 print(chosen_word)
+print('Welcome to Hangman game! You have 5 lives to complete')
 
-guess = str(input('Choose a letter: ')).lower()
-for i in range(0, len(chosen_word) - 1):
-    if guess in chosen_word_list:
-        try:
-            index = chosen_word_list.index(guess, i)
-        except ValueError:
-            continue
-        i += 1
-        display[index] = guess
-    else:
-        print('Wrong!')
-        break
+while '_' in display and lives > 0:
+    # Ask for a letter to the user
+    guess = str(input('Choose a letter: ')).lower()
+    # Start a loop in the length of the word to see if the letter is in
+    # more than one position
+    for i in range(0, len(chosen_word) - 1):
+        if guess in chosen_word_list:
+            try:
+                index = chosen_word_list.index(guess, i)
+            except ValueError:
+                continue
+            display[index] = guess
+            print(''.join(display))
+        else:
+            print(''.join(display))
+            lives -= 1
+            if lives == 0:
+                print('GAME OVER')
+                break
+            print(f'{lives} lives remaining')
+            break
 print(display)
