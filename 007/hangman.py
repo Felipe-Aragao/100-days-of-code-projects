bb618de837386d182b7aea86a4577bae803a011b
Felipe Aragão <lipelima2006@gmail.com>
2022-09-04 21:42:21 -0300

    completed hangman.py game
    Add comments
    Add two new files that contain the word list and the drawings

--- 007/hangman.py
+++ 007/hangman.py
@@ -1,20 +1,23 @@
 import random
+import hangman_stages as stage  # Drawings of the stickman
+import word_list as wl # Word list
 
 # Create initial variables and lists
-word_list = ["aardvark", "baboon", "camel"]
+word_list = wl.word_list
 chosen_word = random.choice(word_list)
 chosen_word_list = list(chosen_word)
 display = list('_' * len(chosen_word))
-lives = 5
-# Debug print()
-print(chosen_word)
-print('Welcome to Hangman game! You have 5 lives to complete')
+lives = 7
+index_stage = 6
 
+# Print introduction to the game
+print(f'Welcome to Hangman game! You have {lives} lives to complete')
+print(f'{len(chosen_word)} letters\n', ''.join(display))
+# Start the game loop that should continue
+# while there is blank spaces and while there is still lives
 while '_' in display and lives > 0:
-    # Ask for a letter to the user
     guess = str(input('Choose a letter: ')).lower()
-    # Start a loop in the length of the word to see if the letter is in
-    # more than one position
+    # Test if the guess is correct
     for i in range(0, len(chosen_word) - 1):
         if guess in chosen_word_list:
             try:
@@ -22,13 +25,21 @@ while '_' in display and lives > 0:
             except ValueError:
                 continue
             display[index] = guess
-            print(''.join(display))
+        # If the guess is incorrect lose a life
         else:
-            print(''.join(display))
+            index_stage -= 1
             lives -= 1
+            # When lives reach 0 it is game over
             if lives == 0:
-                print('GAME OVER')
+                print('\nGAME OVER')
+                print(f'The correct word was {chosen_word.upper()}')
                 break
             print(f'{lives} lives remaining')
             break
-print(display)
+    print(f'You guessed {guess}')
+    print('\n', ''.join(display))
+    if lives > 0:
+        print(stage.stages[index_stage], '\n')
+# See if the user guessed all letters
+if '_' not in display:
+    print('Congratulation you WON!')
