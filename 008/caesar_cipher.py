8f59eb0040b7220da20a9c0de08d8f139ef0c3ac
Felipe Aragão <lipelima2006@gmail.com>
2022-09-26 18:05:21 -0300

    removed repetition

--- 008/caesar_cipher.py
+++ 008/caesar_cipher.py
@@ -1,4 +1,4 @@
-def encrypt(txt, shift):
+def caesar(txt, shift, direction):
     new_msg_list = []
     for i in txt:
         try:
@@ -7,29 +7,19 @@ def encrypt(txt, shift):
             new_msg_list.append(' ')
             continue
         if shift + index <= 25:
-            new_msg_list.append(alphabet[index + shift])
+            if direction == 'encode':
+                new_msg_list.append(alphabet[index + shift])
+            else:
+                new_msg_list.append(alphabet[index - shift])
         else:
-            new_shift = shift - (25 - index)
-            index = 0
-            new_msg_list.append(alphabet[(index + new_shift) - 1])
-    new_msg = ''.join(new_msg_list)
-    print(new_msg)
-
-
-def decrypt(txt, shift):
-    new_msg_list = []
-    for i in txt:
-        try:
-            index = alphabet.index(i)
-        except ValueError:
-            new_msg_list.append(' ')
-            continue
-        if shift + index <= 25:
-            new_msg_list.append(alphabet[index - shift])
-        else:
-            new_shift = shift + (25 - index)
-            index = 25
-            new_msg_list.append(alphabet[(index - new_shift)])
+            if direction == 'encode':
+                new_shift = shift - (25 - index)
+                index = 0
+                new_msg_list.append(alphabet[(index + new_shift) - 1])
+            else:
+                new_shift = shift + (25 - index)
+                index = 25
+                new_msg_list.append(alphabet[(index - new_shift)])
     new_msg = ''.join(new_msg_list)
     print(new_msg)
 
@@ -46,7 +36,4 @@ while True:
         break
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
+caesar(text, shift, direction)
